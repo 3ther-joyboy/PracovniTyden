b30d50d63680aff49c9698986196281506055411
22028td <22028td@creativehill.cz>
2025-02-18 11:27:22 +0100

    awdawd

--- src/main/java/pup/quiz/server/model/Session.java
+++ src/main/java/pup/quiz/server/model/Session.java
@@ -1,4 +1,18 @@
 package pup.quiz.server.model;
+import java.security.SecureRandom;
 
 public class Session {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static String generateCode() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder code = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            code.append(CHARACTERS.charAt(index));
+        }
+
+        return code.toString();
+    }
 }
--- src/main/java/pup/quiz/server/restcontroller/index.java
+++ src/main/java/pup/quiz/server/restcontroller/index.java
@@ -1,10 +1,17 @@
 package pup.quiz.server.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pup.quiz.server.model.Punishments;
+import pup.quiz.server.repo.PunishmentRepo;
 
 @RestController
 @RequestMapping("/servertest")
 public class index {
+
+    @Autowired
+    private PunishmentRepo databazeTable;
+
     @GetMapping(value = "/")
     public String HelloWorld() {
         return "ahoj pusinko";
@@ -18,6 +25,10 @@ public class index {
     @PostMapping(value = "/{name}")
     public void jmnjno(@PathVariable(name = "name") String jmenoHrace) {
         System.out.printf(jmenoHrace);
-        //adw
+
+        Punishments save = new Punishments();
+        save.Name = jmenoHrace;
+        databazeTable.save(save);
     }
+
 }
