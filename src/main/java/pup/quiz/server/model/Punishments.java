51b17d2b3ebb7ae902f9fd5b8f70f0dc445b309b
22013lv <22013lv@creativehill.cz>
2025-02-18 10:00:09 +0100

    server settings, punishment setup

--- src/main/java/pup/quiz/server/model/Punishments.java
+++ src/main/java/pup/quiz/server/model/Punishments.java
@@ -1,4 +1,24 @@
 package pup.quiz.server.model;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "punishments")
 public class Punishments {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long Id;
+
+    @Column(name = "name")
+    public String Name;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "p_sets",
+            joinColumns = @JoinColumn(name = "punishment"),
+            inverseJoinColumns = @JoinColumn(name = "set")
+    )
+    public Set<PunishmentsSets> Sets;
 }
--- src/main/java/pup/quiz/server/model/PunishmentsSets.java
+++ src/main/java/pup/quiz/server/model/PunishmentsSets.java
@@ -1,4 +1,10 @@
 package pup.quiz.server.model;
 
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
+
 public class PunishmentsSets {
+    @ManyToMany(mappedBy = "Sets")
+    public Set<Punishments> Punish;
 }
