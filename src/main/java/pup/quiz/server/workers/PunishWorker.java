e669598d92ad43f31dd29397c8bb212d446b0cf2
22013lv <22013lv@creativehill.cz>
2025-02-20 10:38:07 +0100

    added pun worker

---
+++ src/main/java/pup/quiz/server/workers/PunishWorker.java
@@ -0,0 +1,16 @@
+package pup.quiz.server.workers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import pup.quiz.server.model.PunishmentsSets;
+import pup.quiz.server.repo.PunismentSetRepo;
+
+public class PunishWorker {
+    @Autowired
+    private static PunismentSetRepo p_rep;
+
+    public static Long CreateSet(String name) {
+        PunishmentsSets set = new PunishmentsSets();
+        set.Name = name;
+        return p_rep.save(set).Id;
+    }
+}
