72d069a4eab33c69c30daa2c5c7e0cce9d08b90b
22028td <22028td@creativehill.cz>
2025-02-20 10:35:52 +0100

    broken databaasaeqwe savejiji

--- src/main/java/pup/quiz/server/restcontroller/Admin.java
+++ src/main/java/pup/quiz/server/restcontroller/Admin.java
@@ -1,9 +1,6 @@
 package pup.quiz.server.restcontroller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pup.quiz.server.model.Question;
 import pup.quiz.server.workers.QuestionWorker;
 
@@ -11,14 +8,17 @@ import pup.quiz.server.workers.QuestionWorker;
 @RequestMapping("/admin")
 public class Admin {
     public static String Password = "1234";
-    @PostMapping(value = "/post_question/{question}/{answer}/{answer_2}/{answer_3}/{answer_4}/{password}")
+    @PostMapping(value = "/post_question/{question}/{answer}/{answer_2}/{answer_3}/{answer_4}/{password}/{set}")
     public Long PostQuestion(@PathVariable(name = "question") String question,
                              @PathVariable(name = "answer") String answer1,
                              @PathVariable(name = "answer_2") String answer2,
                              @PathVariable(name = "answer_3") String answer3,
                              @PathVariable(name = "answer_4") String answer4,
-                             @PathVariable(name = "password") String password
+                             @PathVariable(name = "password") String password,
+                             @PathVariable(name = "set") Long set
     ) {
+
+        System.out.printf("awdwadwa");
         if(Password.equals(password)) {
             System.out.printf("Question: " + question + "\n");
             System.out.printf("Answer 1: " + answer1 + "\n");
@@ -26,7 +26,9 @@ public class Admin {
             System.out.printf("Answer 3: " + answer3 + "\n");
             System.out.printf("Answer 4: " + answer4 + "\n");
 
-            return QuestionWorker.CreateQuestion(question, new String[]{answer1, answer2, answer3, answer4}, new boolean[]{true, false, false, false});
+
+
+            return QuestionWorker.CreateQuestion(question, new String[]{answer1, answer2, answer3, answer4}, new boolean[]{true, false, false, false}, set);
         }
         return 0L;
     }
